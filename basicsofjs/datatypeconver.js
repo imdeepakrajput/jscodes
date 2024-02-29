6f4f5c69b5053f517b0fdf131b261ef5267669b3
imdeepakrajput <161502175+imdeepakrajput@users.noreply.github.com>
2024-02-29 06:02:09 +0000

    datatypeconver.js

---
+++ basicsofjs/datatypeconver.js
@@ -0,0 +1,33 @@
+let score = "deepak Rajput"
+
+// console.log(typeof score)  // => first way to find out type of data.
+// console.log(typeof(score)) // => second way to find out type of data.
+
+let valueNumber = Number(score)
+
+//  console.log(typeof valueNumber)
+//  console.log(valueNumber)
+
+
+// "33"  => 33
+// "33abc"  => Nan
+//  true  => 1; false => 0
+
+
+let isLoggedIn = "deepak"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+
+// 1 => true; 0 => false
+// "" => false
+// "deepak"  => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
